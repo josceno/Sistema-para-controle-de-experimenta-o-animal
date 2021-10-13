48644b0ed30118c41facd92801f4e82b97f285b2
xxtheexx <71050375+xxtheexx@users.noreply.github.com>
2021-10-13 18:29:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -42,13 +42,16 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Acesse sua conta</h1>
-    <form action="" method="POST">
+    <meta name="viewports" content="width=device-width, initial-scale-1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>login</title>
+    </head>
+    <body>
+    <main class="login">
+        <div class="login__container">
+            <h1 class="login__title">login do usuario</h1>
+            
+                <form action="" method="POST">
         <p>
             <label>E-mail</label>
             <input type="text" name="email">
@@ -58,8 +61,10 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
             <input type="password" name="senha">
         </p>
         <p>
-            <button type="submit">Entrar</button>
+        <button class="login__submit">login</button>
+                <a class="login__reset" href="#">esqueci a senha</a>
         </p>
-    </form>
+            </form>
+        </div>
+    </main>
 </body>
-</html>
